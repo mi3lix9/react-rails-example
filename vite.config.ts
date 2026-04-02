17999e3cff390e9eeca069a2d13378d80b2ebfb2
Ali ALshaikh <mi3lix9@outlook.sa>
2026-04-02 17:54:55 +0300

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import path from "path";
 import { defineConfig } from "vite";
 import ViteRuby from "vite-plugin-ruby";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [ViteRuby(), react()],
+  plugins: [ViteRuby(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app/frontend"),
+    },
+  },
 });
