f3353a71afb8f02ad09be728402683084af6edb7
Ali ALshaikh <mi3lix9@outlook.sa>
2026-04-02 18:03:48 +0300

    feat: migrate to Inertia.js, remove API layer and React Router

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  namespace :api do
-    resources :products, only: [:index, :show, :create, :update, :destroy]
-  end
-
-  root "pages#index"
-  get "*path", to: "pages#index", via: :all
+  resources :products
+  root "products#index"
 end
