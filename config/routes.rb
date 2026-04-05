1fc13b1d62e35364d888f4f8cd6eec726611d7a6
Ali ALshaikh <mi3lix9@outlook.sa>
2026-04-05 13:17:46 +0300

    feat: add cart, checkout, orders, and admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   root "products#index"
   resources :products
+
+  get "cart", to: "cart#index"
+  get "checkout", to: "checkout#index"
+  resources :orders, only: [ :create, :show ], param: :token
+
+  namespace :admin do
+    resources :orders, only: [ :index, :show, :update ]
+  end
 end
